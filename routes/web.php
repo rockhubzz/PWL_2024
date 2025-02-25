069e33ed88502ce8a118bb8e58bb552d8dbe1156
Rocky Alessandro <compatiblewindows7@gmail.com>
2025-02-25 22:33:28 +0700

    modifikasi pagecontroller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,22 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+Route::get('/', [PageController::class,'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class,'articles']); 
 
-Route::get('/hello', function () {
-    return 'Hello World';
-    });
+Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::get('/world', function () {
     return 'World';
     });
 
-Route::get('/about', function () {
-    return nl2br("NIM: 2341720197\n
-    Nama: Rocky Alessandro Kristanto");
-    });
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya '.$name;
@@ -38,9 +34,6 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
     });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman artikel dengan ID '.$id;
-    });
 
 // Route::get('/user/{name?}', function ($name=null){
 //     return 'Nama saya '.$name;
